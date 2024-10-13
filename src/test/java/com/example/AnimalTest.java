178e8afde18138e4b9e16897576b0e7bc8ce7c80
Sergej Skomorokhov <Kanye29@yandex.ru>
2024-10-13 18:14:29 +0500

    The project is assembled, the coverage is one hundred percent

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -1,4 +1,47 @@
 package com.example;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+@RunWith(Parameterized.class)
 public class AnimalTest {
+    private final String animalKind;
+    private final List<String> food;
+
+    public AnimalTest(String animalKind, List<String> food) {
+        this.animalKind = animalKind;
+        this.food = food;
+    }
+
+    @Parameterized.Parameters(name = "Тип животного: {0}, Что ест: {1}")
+    public static Object[][] getCredentials() {
+        return new Object[][] {
+                { "Травоядное", List.of("Трава", "Различные растения")},
+                { "Хищник", List.of("Животные", "Птицы", "Рыба")},
+        };
+    }
+
+    @Test
+    public void getFamilyTest() {
+        Animal animal = new Animal();
+        assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи",
+                animal.getFamily());
+    }
+
+    @Test
+    public void checkExceptionInGetFood() {
+        Animal animal = new Animal();
+        Exception exception = assertThrows(Exception.class, () -> animal.getFood("Обезьяна"));
+        assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник",
+                exception.getMessage());
+    }
+    @Test
+    public void testGetFood() throws Exception {
+        Animal animal = new Animal();
+        assertEquals(food, animal.getFood(animalKind));
+    }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,4 +1,58 @@
 package com.example;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(Parameterized.class)
 public class LionTest {
+    private final String sex;
+    private final boolean havMane;
+
+    Feline feline = new Feline();
+
+    public LionTest(String sex, boolean havMane) {
+        this.sex = sex;
+        this.havMane = havMane;
+    }
+
+    @Parameterized.Parameters(name = "Пол: {0}, Наличие гривы: {1}")
+    public static Object[][] getCredentials() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Test
+    public void testHasMane() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(havMane, lion.hasMane());
+    }
+
+    @Test
+    public void testGetKittens() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        assertEquals(feline.getKittens(), lion.getKittens());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        assertEquals(feline.getFood("Хищник"), lion.getFood());
+    }
+    @Test
+    public void testLionConstructor_InvalidSex_ThrowsException() {
+        try {
+            new Lion("Недопустимый пол", feline);
+            fail("Ожидалось исключение для недопустимого пола");
+        } catch (Exception e) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
+        }
+    }
 }
+
+
