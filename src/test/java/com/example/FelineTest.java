473eac00bc3cb80095f059f32863f5e24fd6e72c
Sergej Skomorokhov <Kanye29@yandex.ru>
2024-10-16 16:41:41 +0500

    Inheritance has been removed

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,17 +1,20 @@
 package com.example;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class FelineTest extends TestCase {
+public class FelineTest {
 
+    @Test
     public void testEatMeat() throws Exception {
         Feline feline = new Feline();
-        assertEquals("Животные, Птицы, Рыба", String.join(", ",
-                feline.eatMeat()));
+        assertEquals("Животные, Птицы, Рыба", String.join(", ", feline.eatMeat()));
     }
 
+    @Test
     public void testGetFamily() {
         Feline feline = new Feline();
         assertEquals("Кошачьи", feline.getFamily());
     }
 }
+
